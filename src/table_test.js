d3e626420d831433ee45e415dc39273f9f5386e6
alorencatto <lorencattoaugusto@gmail.com>
2021-03-10 07:10:18 -0300

    Tabela dinamica criada

--- src/table_test.js
+++ src/table_test.js
@@ -3,42 +3,19 @@ import { DataGrid } from '@material-ui/data-grid';
 
 import ListRowComponent from './ListRowComponents';
 
-
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'Item_name', headerName: 'Item name', width: 130 },
-  // { field: 'lastName', headerName: 'Last name', width: 130 },
-  // {
-  //   field: 'age',
-  //   headerName: 'Age',
-  //   type: 'number',
-  //   width: 90,
-  // },
-  // {
-  //   field: 'fullName',
-  //   headerName: 'Full name',
-  //   description: 'This column has a value getter and is not sortable.',
-  //   sortable: false,
-  //   width: 160,
-  //   valueGetter: (params) =>
-  //     `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
-  // },
-];
-
-// const rows = [
-//   { id: 1, lastName: 'Snow', Item_name: 'Jon', age: 35 },
-//   { id: 2, lastName: 'Lannister', Item_name: 'Cersei', age: 42 },
-//   { id: 3, lastName: 'Lannister', Item_name: 'Jaime', age: 45 },
-//   { id: 4, lastName: 'Stark', Item_name: 'Arya', age: 16 },
-//   { id: 5, lastName: 'Targaryen', Item_name: 'Daenerys', age: null },
-//   { id: 6, lastName: 'Melisandre', Item_name: null, age: 150 },
-//   { id: 7, lastName: 'Clifford', Item_name: 'Ferrara', age: 44 },
-//   { id: 8, lastName: 'Frances', Item_name: 'Rossini', age: 36 },
-//   { id: 9, lastName: 'Roxie', Item_name: 'Harvey', age: 65 },
+// const columns = [
+//   { field: 'id', headerName: 'ID', width: 70 },
+//   { field: 'firstName', headerName: 'First name', width: 130 },
+//   { field: 'lastName', headerName: 'Last name', width: 130 },
+  
 // ];
 
-
-
+// const rows = [
+//   { id: 1, lastName: 'Snow' },
+//   { id: 2, lastName: 'Lannister' },
+//   { id: 3, lastName: 'Lannister' },
+  
+// ];
 
 // export default function DataTable() {
 //   return (
@@ -66,30 +43,45 @@ export default class DataTable extends React.Component {
     var url = "http://127.0.0.1:8000/list/";
     const response =await fetch(url,config);
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     this.setState({lists:data,loading:false});
-
-  
-
-
   }
 
   render(){
 
+    const columns = [
+      { field: 'id', headerName: 'ID', width: 70 },
+      { field: 'name', headerName: 'Nome da Lista', width: 180 },
+    ];
+
+    const rows_original = [
+            { id: 1, lastName: 'Snow' },
+            { id: 2, lastName: 'Lannister' },
+            { id: 3, lastName: 'Lannister' },
+            
+          ];
+
     const listsApi = this.state.lists;
-    const rows = [listsApi.map(list=> <ListRowComponent id={list.id} name={list.name}/>)]
+    // const rows = [listsApi.map(list=> <ListRowComponent id={list.id} name={list.name}/>)]
 
-    console.log(rows);
+    // const rows = [{'id':'','name': ''}];
+
+    const rows = [];
+
+    for( var key in listsApi){
+      var value = listsApi[key]
+      rows.push({'id':value.id,'name':value.name});
+    }
+
+    console.log(rows)
+
+
+
     return (
-      // <div style={{ height: 400, width: '100%' }}>
-      //   <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
-      // </div>
-
-            <div>
-                 {/* <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection /> */}
-                 <span>Fazendo ainda</span>
-            </div>
-    );
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid rows={rows} columns={columns} pageSize={5} checkboxSelection />
+      </div>
+    )
   }
 
   
